f95c4221fdaf053ac4f85c9b511d9be947880fb5
Sadon <godfreyb585+github@gmail.com>
2023-09-30 20:32:28 +0800

    1.0.0.4.55

--- this.js
+++ this.js
@@ -2,28 +2,13 @@ const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
 
-toggleBtn.onclick = function() { dropDownMenu.classList.toggle('open')
+toggleBtn.onclick = function() { 
+    dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
 
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
-}
-
-function scrollDetect(){
-    var lastScroll = 0;
-
-    window.onscroll = function() {
-        let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-
-        if (currentScroll > 150) {
-            lastScroll = currentScroll;
-            document.getElementById("")
-        } else {
-            document.getElementById("")
-        }
-    };
-    scrollDetect();
 }
 
 const observer = new IntersectionObserver((entries) => {
