a59d58a442fdedbfb0aedb71d524fa453da9c742
Sadon <godfreyb585+github@gmail.com>
2023-09-30 20:42:19 +0800

    1.0.0.57
    changes on js

--- this.js
+++ this.js
@@ -1,26 +1,12 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
 
-toggleBtn.onclick = function() { 
-    dropDownMenu.classList.toggle('open')
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.set('open')
     const isOpen = dropDownMenu.classList.contains('open')
 
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
-}
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+};
