838c074c6f40fe8924c3679b716163bd3f7e44ed
Sadon <godfreyb585+github@gmail.com>
2023-09-30 15:20:09 +0800

    1.0.0.2

--- this.js
+++ this.js
@@ -10,3 +10,20 @@ toggleBtn.onclick = function () {
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+function scrollDetect(){
+    var lastScroll = 0;
+
+    window.onscroll = function() {
+        let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (currentScroll > 150) {
+            lastScroll = currentScroll;
+            document.getElementById("")
+        } else {
+            document.getElementById("")
+        }
+    };
+    scrollDetect();
+}
+
